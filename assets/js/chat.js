6cfaeb1e7aa179f7a67d5e1f81c84ca3537b291c
zach42o1 <48109689+zach42o1@users.noreply.github.com>
2020-04-22 15:08:49 -0600

    forth commit

---
+++ assets/js/chat.js
@@ -0,0 +1,16 @@
+$('.submit').click(function() {
+    newMessage();
+});
+
+//not working but only a matter of format
+/*
+function newMessage() {
+    message = $(".message-input input").val();
+    if($.trim(message) == '') {
+        return false;
+    }
+    $('<li class="sent"><img src="http://emilcarlsson.se/assets/mikeross.png" alt="" /><p>' + message + '</p></li>').appendTo($('.messages ul'));
+    $('.message-input input').val(null);
+    $('.contact.active .preview').html('<span>You: </span>' + message);
+    $(".messages").animate({ scrollTop: $(document).height() }, "fast");
+};*/
