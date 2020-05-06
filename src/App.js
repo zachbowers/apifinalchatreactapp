5d8dc072e5ecc8e53289f55e814d4eecd81841a8
Derek Gunnels <45681663+DGunnels@users.noreply.github.com>
2020-05-06 17:00:47 -0600

    Quick push to fix

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
 import React from "react";
-import ChatApp from './components/ChatApp.js';
+import chatApp from './components/ChatApp.js';
 import Auth from './components/auth.js';
+import NavHeader from './components/navheader.js';
 import './App.css';
 import { HashRouter, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './stores/stores.js';
 import AppBar from '@material-ui/core/AppBar';
-import NavHeader from "./components/navheader";
-import {StreamChat} from "stream-chat";
-function App() {
-    return (
+
+import { Chat, Channel, ChannelHeader, Window } from 'stream-chat-react';
+import { MessageList, MessageInput, MessageLivestream } from 'stream-chat-react';
+import { MessageInputSmall, Thread } from 'stream-chat-react';
+import { StreamChat } from 'stream-chat';
+
+import 'stream-chat-react/dist/css/index.css';
+
+const chatClient = new StreamChat('exchr7a3dmm3');
+const userToken = 'myusertoken';
+
+chatClient.setUser(
+    {
+        id: 'Gentle-mode-1',
+        name: '',
+        image: 'https://getstream.io/random_svg/?id=gentle-mode-1&name=Gentle+mode'
+    },
+    userToken,
+);
+
+const channel = chatClient.channel('livestream', 'webapi', {
+    name: 'OurGlass',
+});
+
+const App = () => (
+    <div className="wrap">
         <div className="siteNav">
             <Provider store={store}>
                 <HashRouter>
@@ -20,24 +43,18 @@ function App() {
                 </HashRouter>
             </Provider>
         </div>
-    )
-}
-export default App;
+        <Chat client={chatClient} theme={'livestream light'}>
+            <Channel channel={channel} Message={MessageLivestream}>
+                <Window hideOnThread>
+                    <ChannelHeader live />
+                    <MessageList />
+                    <MessageInput Input={MessageInputSmall} focus />
+                </Window>
+                <Thread fullWidth />
+            </Channel>
+        </Chat>
+    </div>
+);
 
-//original
-/*function App() {
-    return (
-        <div className="siteNav">
-            <Provider store={store}>
-                <HashRouter>
-                    <div className="NavBar">
-                        <AppBar />
-                        <Route exact path="/" render={() => <chatApp />} />
-                        <Route exact path="/signin" render={() => <Auth />} />
-                    </div>
-                </HashRouter>
-            </Provider>
-        </div>
-    )
-}
-export default App;*/
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import ChatApp from './ChatApp';
-import * as serviceWorker from './serviceWorker';
-import App from './App'
+import './App.css';
+import App from './App';
+import * as serviceWorker from './serviceWork';
 
 ReactDOM.render(
-    <App>
-        <ChatApp />
-    </App>,
+    <App />,
     document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
