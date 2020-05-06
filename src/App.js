f4d6289d12202d5a6840a5d7f30febf272f4493a
zach42o1 <48109689+zach42o1@users.noreply.github.com>
2020-05-06 14:32:53 -0600

    merge

--- src/App.js
+++ src/App.js
@@ -6,12 +6,30 @@ import { HashRouter, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './stores/stores.js';
 import AppBar from '@material-ui/core/AppBar';
-function App() {
+import NavHeader from "./components/navheader";
+/*function App() {
     return (
         <div className="siteNav">
             <Provider store={store}>
                 <HashRouter>
                     <div className="NavBar">
+                        <AppBar />
+                        <Route exact path="/" render={() => <chatApp />} />
+                        <Route exact path="/signin" render={() => <Auth />} />
+                    </div>
+                </HashRouter>
+            </Provider>
+        </div>
+    )
+}
+export default App;*/
+function App() {
+    return (
+        <div className="siteNav">
+            <Provider store={store}>
+                <HashRouter>
+                    <NavHeader />
+                    <div className="navbar navbar-dark align-items-start sidebar sidebar-dark accordion bg-gradient-primary p-0" id="nav1bar">
                         <AppBar />
                         <Route exact path="/" render={() => <chatApp />} />
                         <Route exact path="/signin" render={() => <Auth />} />
---
+++ src/components/navheader.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import { Navbar, NavItem, Nav } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
+import { logoutUser } from "../actions/authActions";
+
+class NavHeader extends Component {
+
+    logout() {
+        this.props.dispatch(logoutUser());
+    }
+
+    render() {
+        return (
+            <div id="wrapper">
+                <nav className="navbar navbar-dark align-items-start sidebar sidebar-dark accordion bg-gradient-primary p-0" id="nav1bar">
+                    <div className="container-fluid d-flex flex-column p-0">
+                        <div className="text-center d-none d-md-inline"></div>
+                        <ul className="nav navbar-nav text-light" id="accordionSidebar">
+                            <li className="nav-item dropdown no-arrow" role="presentation">
+                                <LinkContainer to="../">
+                                    <img id="brandlogo" src="assets/img/light.png" />
+                                </LinkContainer>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+                <nav className="navbar navbar-dark align-items-start sidebar sidebar-dark accordion bg-gradient-primary p-0" id="navbar2">
+                    <div className="container-fluid d-flex flex-column p-0">
+                        <a className="navbar-brand d-flex justify-content-center align-items-center sidebar-brand m-0" id="brandHeader">
+                            <LinkContainer to="../">
+                                <div className="sidebar-brand-text mx-3"><span>ourglass</span></div>
+                            </LinkContainer>
+                        </a>
+                        <hr className="sidebar-divider my-0" > </hr>
+                        <ul className="nav navbar-nav text-light" id="accordionSidebar"> </ul>
+                    </div>
+
+                </nav>
+            </div>
+
+
+
+        );
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        loggedIn: state.auth.loggedIn,
+        username: state.auth.username,
+        selectedMovie: state.movie.selectedMovie,
+    }
+}
+
+export default withRouter(connect(mapStateToProps)(NavHeader));
